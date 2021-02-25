b8b25f4eba3ba9ee652a172c67a1a2c4b5b7f1a8
ENGI_Tsuyuzaki <tsuyuzaki.tatsuya@engi-st.jp>
2021-02-25 16:02:08 +0900

    位置をバウンディングボックスで判定するように変更

--- mainEdit/sideCheck.py
+++ mainEdit/sideCheck.py
@@ -3,33 +3,18 @@ import maya.cmds as cmds
 objs = cmds.ls(sl=True)
 
 def auto_side(obj,dist=0):
-    get_transX = cmds.xform(obj,q=True,t=True,ws=True)[0]
-    get_transX = round(get_transX,3)
-    if get_transX > dist:
-        left_objs.append(obj)
-    elif get_transX < -dist:
-        right_objs.append(obj)
-    else:
-        center_objs.append(obj)
-        
-def is_side(obj,side):
-    if side in obj:
-        return obj
-    else:
-        auto_side(obj)
+    left = []
+    right = []
+    center = []
+    for obj in objs:
+        boundingBox = cmds.xform(obj,q=True,boundingBox=True,ws=True)
+        minX = round(boundingBox[0],3)
+        maxX = round(boundingBox[3],3)
+        if minX > dist and maxX > dist:
+            left.append(obj)
+        elif minX < -dist and maxX < -dist:
+            right.append(obj)
+        else:
+            center.append(obj)
 
-center_objs = []
-right_objs = []
-left_objs = []
-
-for obj in objs:
-    if is_side(obj,"C"):
-        center_objs.append(obj)
-    elif is_side(obj,"R"):
-        right_objs.append(obj)
-    elif is_side(obj,"L"):
-        left_objs.append(obj)
-
-print('center',set(center_objs))
-print('left',set(left_objs))
-print('right',set(right_objs))
+    return left,right,center
